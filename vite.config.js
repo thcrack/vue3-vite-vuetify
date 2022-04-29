7901b5a9305a50edb7d00991dcfffc5384b3f70d
Carolyn Yu <carolyn@Carolyns-MBP.lan>
2022-04-29 17:06:16 -0400

    git path

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  // base: "vue3-vite-vuetify"
+  base: "vue3-vite-vuetify"
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
